449135203fc4f525533fa25e72a8c638039a2dec
ramaATgit <ramamir23@hotmail.co.uk>
2019-04-29 14:36:25 +0100

    mnyTOsql Screipts & notes

---
+++ mny2sql-repo/cleancsv.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ # NOW=`date +%d%b%y`
+
+sed -e '/Bank and Cash Accounts/,$!d' -e '/Total Bank and Cash Accounts/,$d' -e 's/Bank and Cash Accounts/Account,bal/'  -e 's/[0-9]..$//g' all.csv > allddMMYY
+
+# sed -e '/Bank and Cash Accounts/,$!d' -e '/Total Bank and Cash Accounts/,$d' -e 's/Bank and Cash Accounts/Account,bal/'  -e 's/[0-9]..$//g' all.csv > all$NOW
